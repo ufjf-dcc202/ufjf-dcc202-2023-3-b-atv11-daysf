12b802125fc1532d26798b2d97de3523b4845246
Yan Dias <13732727750@estudante.ufjf.br>
2023-11-21 22:29:31 -0300

    criei funções

---
+++ listas.js
@@ -0,0 +1,23 @@
+const Lista = [
+    "Um",
+    "Dois",
+    "Três"
+];
+
+function getLista(){
+    return Lista;
+}
+
+function adicionaNaLista(){
+
+}
+
+function removeDaLista(){
+
+}
+
+function limpaLista(){
+
+}
+
+export {getLista, adicionaNaLista,removeDaLista, limpaLista}
