73498573209ecc76c999f9b6c28f1f2d6b066011
Yan Dias <13732727750@estudante.ufjf.br>
2023-11-21 23:22:50 -0300

    corrigi erros

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-import { getLista } from "./src/lista.js";
+import { limpaLista, getLista, adicionaNaLista } from "./src/lista.js";
 
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
-const btnLImpar = document.querySelector("#limpar");
+const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
 atualizarListaOrdenada();
 
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+
+btnLimpar.addEventListener('click', limparListaOrdenada);
+
+function limparListaOrdenada(){
+    limpaLista();
+    atualizarListaOrdenada();
+}
+
+function adicionaItemDaEntrada (){
+    const valor = pEntrada.textContent;
+    adicionaNaLista(valor);
+    pEntrada.textContent="";
+    atualizarListaOrdenada();
+}
+
 function atualizarListaOrdenada(){
     const lista = getLista();
+    olItens.innerHTML ="";
     for (let i = 0; i < lista.length; i++){
         adicionaElementoNaListaOrdenada(lista[i]);
     }
 }
 
-function adicionaElementoNaLista(texto){
+function adicionaElementoNaListaOrdenada(texto){
 const li = document.createElement("li");
 li.textContent = texto;
 olItens.appendChild(li);
