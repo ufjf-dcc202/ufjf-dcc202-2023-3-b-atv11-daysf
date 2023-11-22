ca1934296605cdc256341015449a34dbff75dde8
Yan Dias <13732727750@estudante.ufjf.br>
2023-11-21 22:51:01 -0300

    add att lista

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+import { getLista } from "./src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLImpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaElementoNaLista("Primeiro");
-adicionaElementoNaLista("Segundo");
-adicionaElementoNaLista("Terceiro");
+atualizarListaOrdenada();
+
+function atualizarListaOrdenada(){
+    const lista = getLista();
+    for (let i = 0; i < lista.length; i++){
+        adicionaElementoNaListaOrdenada(lista[i]);
+    }
+}
 
 function adicionaElementoNaLista(texto){
 const li = document.createElement("li");
